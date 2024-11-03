5463bc935db4512a13dcf2201f9ed6c63aa51dac
kohicha <yensyd.francisco@gmail.com>
2024-11-03 14:44:44 +0800

    edited config file

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import type { Site, Ui, Features } from './types'
 
 export const SITE: Site = {
-  website: 'https://astro-antfustyle-theme.vercel.app/',
+  website: 'https://dombusters.github.io/',
   base: '/',
   title: 'DOMBUSTERS Official',
   description:
